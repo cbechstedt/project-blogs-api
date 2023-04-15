e41b15b5c6ab8db43128519bed028da296146b64
Christian <chrisnebech@hotmail.com>
2023-04-15 17:07:20 -0300

    requisito 6 lint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -34,14 +34,14 @@ const getUsers = async (req, res) => {
 
 const getUserById = async (req, res) => {
   const { id } = req.params;
-  const { type, message } = await userService.getUserById(id)
+  const { type, message } = await userService.getUserById(id);
 
   if (type) {
     return res.status(404).json({ message });
   }
 
   return res.status(200).json(message);
-}
+};
 
 module.exports = {
   login,
